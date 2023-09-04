8ad9e461c3a2d46fdabb71145629601f1222968f
amandeepkaur13akl <143216814+amandeepkaur13akl@users.noreply.github.com>
2023-09-04 21:39:29 +0530

    fibonacci_series.py

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,9 +1,9 @@
-n=50
+no=50
 num1 = 0
 num2 = 1
 next_number = num1
 
-while next_number <= n:
+while next_number <= no:
 	print(next_number, end=" \n")
 	num1, num2 = num2, next_number
 	next_number = num1 + num2
